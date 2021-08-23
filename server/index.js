1e2616d9e4876652470c1960727c350ee73980ea
Vedank022 <vedankn01@gmail.com>
2021-08-24 00:54:03 +0530

    delpoy setup

--- server/index.js
+++ server/index.js
@@ -9,16 +9,14 @@ app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/todos", todosRoutes);
-const mongodb =
-  "mongodb+srv://user1:qawsedrf@cluster0.wtf9b.mongodb.net/todo-db?retryWrites=true&w=majority";
 app.get("/", (req, res) => {
   res.send("Welcome to server");
 });
 const PORT = process.env.PORT || 5000;
 mongoose
-  .connect(mongodb, { useUnifiedTopology: true, useNewUrlParser: true })
+  .connect(process.env.mongodb, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => {
     console.log("Connected");
-    app.listen(5000);
+    app.listen(PORT);
   })
   .catch((err) => console.log(err));
